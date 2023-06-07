fddd4598ad2deaafa01aca25a742c9e3fa312b02
dginovker <danielginovker@gmail.com>
2023-06-07 16:46:28 +0900

    Add FAQ

--- src/pages/splash/index.tsx
+++ src/pages/splash/index.tsx
@@ -3,97 +3,124 @@ import { Link } from "react-router-dom";
 import { Button, Container, Row, Col, Image } from "react-bootstrap";
 import "./index.css";
 
-function imageBanner(url) {
-  return (
-    <Image
-      className="d-block pb-3 pl-1 pr-1"
-      style={{
-        width: "150px",
-        height: "100px",
-      }}
-      src={url}
-      alt="Banner images"
-      rounded
-    />
-  );
-}
-
 function Splash() {
   return (
     <main>
       <div>
-          <div
-            className="jumbotron"
-            style={{
-              backgroundColor: "#F8F9FA",
-            }}
-          >
-            <Container>
-              <Row>
-                <Col md={{ span: 1 }}></Col>
-                <Col md={{ span: 10 }}>
-                  <strong><h1 className="d-flex justify-content-center" style={{fontSize: "5rem"}}>
+        <div
+          className="jumbotron"
+          style={{
+            backgroundColor: "#F8F9FA",
+          }}
+        >
+          <Container>
+            <Row>
+              <Col md={{ span: 1 }}></Col>
+              <Col md={{ span: 10 }}>
+                <strong>
+                  <h1
+                    className="d-flex justify-content-center"
+                    style={{ fontSize: "5rem" }}
+                  >
                     50$
-                  </h1></strong>
-                  <div className="text-muted d-flex justify-content-center pb-2">No Nonsense. No Hidden Fees.</div>
-                  <div className="d-flex justify-content-center">
-                    <Link to="/lessons">
-                      <Button size="lg" className="mr-3 px-5">
-                        Get started
-                      </Button>
-                    </Link>
-                  </div>
-                </Col>
-                <Col md={{ span: 1 }}></Col>
-              </Row>
-            </Container>
+                  </h1>
+                </strong>
+                <div className="text-muted d-flex justify-content-center pb-2">
+                  No Nonsense. No Hidden Fees.
+                </div>
+                <div className="d-flex justify-content-center">
+                  <Link to="/lessons">
+                    <Button size="lg" className="mr-3 px-5">
+                      Get started
+                    </Button>
+                  </Link>
+                </div>
+              </Col>
+              <Col md={{ span: 1 }}></Col>
+            </Row>
+          </Container>
         </div>
       </div>
 
       <Container>
         <Row>
-          <Col sm>
-            <h2>Content you love</h2>
-            <p>
-              We all know learning a language is hard. Sometimes you just need a
-              little payoff for your learning - that's where we come in.
-            </p>
-            <p>
-              If you listen to music in your new language, this is the solution
-              for you. Easier and more meaningful lessons based on your favorite
-              songs.
-            </p>
+          <Col md={{ span: 1 }}></Col>
+          <Col md={{ span: 10 }}>
+            <h2>FAQ</h2>
+            <ul>
+              <li className="pb-2">
+                Why is it so expensive?
+                <ul>
+                  <li>
+                    The price is a deterrent to avoid too many submissions. This
+                    service was originally free and provided on various social
+                    platforms including Reddit, Lemmy, and Blind, but quickly
+                    grew too large.
+                  </li>
+                </ul>
+              </li>
+              <li className="pb-2">
+                What does the service offer?
+                <ul>
+                  <li>
+                    The service has two core parts:
+                    <ol>
+                      <li>
+                        Resume content feedback (format, skills, education,
+                        anything else)
+                      </li>
+                      <li>
+                        Experience (+ Projects) bullet points Direct feedback on
+                        BS detection and improvement areas Example bullet points
+                        rewriting Most users require help in both areas.
+                      </li>
+                    </ol>
+                  </li>
+                </ul>
+              </li>
+              <li className="pb-2">
+                What sort of resumes are accepted?
+                <ul>
+                  <li>
+                    Only Software Engineering resumes are accepted at this time.
+                    Devops & Web Dev focused resumes are accepted too, but not
+                    the subject matter expertise.
+                  </li>
+                </ul>
+              </li>
+              <li className="pb-2">
+                Any example reviews?
+                <ul>
+                  <li>
+                    Yes, see here:{" "}
+                    <a href="https://www.levels.fyi/community/thread/GXiJUI/free-resume-review">
+                      https://www.levels.fyi/community/thread/GXiJUI/free-resume-review
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li className="pb-2">
+                Are you a recruiter for FAANG?
+                <ul>
+                  <li>No.</li>
+                </ul>
+              </li>
+            </ul>
+            <h3>Refund Policy</h3>
+            <ul>
+              <li>
+                Refund Policy:
+                <ul>
+                  <li>
+                    Full refunds are available if you’re not satisfied with the
+                    service (note you will be barred from using the service
+                    again in the future)
+                  </li>
+                </ul>
+              </li>
+            </ul>
           </Col>
-
-          <Col sm>
-            <h2>Spaced Repetition</h2>
-            <p>
-              OurKorean uses a research-backed spaced repeititon learning
-              algorithm - meaning you get the most from your effort.
-            </p>
-            <p>
-              Both spaced repetition and memorable sentences are the best ways
-              to learn new vocab - which is why learning through Music so
-              powerful.
-            </p>
-            <p />
-          </Col>
-
-          <Col sm>
-            <h2>Community Driven</h2>
-
-            <p>
-              The content & lessons are created by the community. Want to add a
-              new song? Sign up and head to the{" "}
-              <Link to="/createlesson">Create Lesson</Link> page.
-            </p>
-            <p>
-              I've created OurKorean simply because there was nothing like it
-              out there - and it'll stay fresh so long as I listen to you.
-              Discord, Reddit, or anywhere else - I'll be reading every
-              suggestion.
-            </p>
-          </Col>
+          <Col md={{ span: 1 }}></Col>
         </Row>
       </Container>
     </main>
