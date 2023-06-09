44958079feed78ff7568b19316aa1053e16c32f8
dginovker <danielginovker@gmail.com>
2023-06-09 12:25:19 -0400

    Improve About section

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -8,35 +8,36 @@ const FAQ = () => {
         <Row>
           <Col md={{ span: 1 }}></Col>
           <Col md={{ span: 10 }}>
-            <h2>FAQ</h2>
+            <h2>About</h2>
             <ul>
+              <li className="pb-2">
+                <div className="pb-2">What does this service offer?</div>
+                The review returns actionable feedback and improvements that can
+                be inserted directly into your resume for better results. This
+                feedback includes:
+                <ul className="pb-2">
+                  <li>Bulletpoint improvements & suggestions</li>
+                  <li>Format/Content improvements</li>
+                  <li>Impact & anti-dilution modifications</li>
+                </ul>
+                This service is for Software Engineering resumes. Other resumes
+                can be submitted, but should include a note for what role they
+                are targetting.
+              </li>
               <li className="pb-2">
                 Why is it so expensive?
                 <ul>
                   <li>
-                    The price is to avoid too many submissions. This service was
-                    originally free, but was flooded with submissions.
+                    The price is a balance between having too many submissions
+                    and paying reviewers fairly.
                   </li>
                 </ul>
-              </li>
-              <li className="pb-2">
-                <div className="pb-2">What does this service offer?</div>
-                The review gives actionable feedback that can, for the most part, be
-                inserted directly into your resume for better results. This feedback
-                includes:
-                <ul className="pb-2">
-                  <li>Bulletpoint guidance & improvements</li>
-                  <li>Format/Content improvements</li>
-                  <li>Impact & anti-dilution modifications</li>
-                </ul>
-                This service is for Software Engineering resumes. Devops & Web
-                Dev resumes can be submitted as well, but are not the main target.
               </li>
               <li className="pb-2">
                 Any example reviews?
                 <ul>
                   <li>
-                    Yes, see here:{" "}
+                    See here:{" "}
                     <a href="https://www.levels.fyi/community/thread/GXiJUI/free-resume-review">
                       https://www.levels.fyi/community/thread/GXiJUI/free-resume-review
                     </a>
