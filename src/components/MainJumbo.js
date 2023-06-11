1d87cd467b0772c538dfc435d694ec47e6c1707d
dginovker <danielginovker@gmail.com>
2023-06-11 16:06:14 -0400

    Move main jumbo onto its own component

---
+++ src/components/MainJumbo.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
+import "../pages/index.css";
+
+function MainJumbo() {
+  return (
+    <div
+      className="jumbotron"
+      style={{
+        backgroundColor: "#F8F9FA",
+      }}
+    >
+      <strong>
+        <h1
+          className="d-flex justify-content-center"
+          style={{ fontSize: "5rem" }}
+        >
+          50$
+        </h1>
+      </strong>
+      <div className="text-muted d-flex justify-content-center pb-2">
+        No Nonsense. No Hidden Fees.
+      </div>
+      <div className="d-flex justify-content-center">
+        <Link to="/checkout">
+          <Button size="lg" className="mr-3 px-5">
+            Get started
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default MainJumbo;
--- src/pages/splash/index.tsx
+++ src/pages/splash/index.tsx
@@ -1,56 +1,20 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { Button, Container, Row, Col } from "react-bootstrap";
 import FAQ from "../../components/FAQ";
 import { logevent } from "../../firebase/firebaseapp";
 import Carousel from "../../components/Carousel";
+import MainJumbo from "../../components/MainJumbo";
 import "../index.css";
 
 function Splash() {
   React.useEffect(() => {
-    logevent("view", {name: window.location.pathname});
+    logevent("view", { name: window.location.pathname });
   }, []);
 
   return (
     <main>
       <div>
         <Carousel />
-        <div className="d-flex justify-content-center p-2 pb-3">
-          <h2>Professional Resume Reviews.</h2>
-        </div>
-        <div
-          className="jumbotron"
-          style={{
-            backgroundColor: "#F8F9FA",
-          }}
-        >
-          <Container>
-            <Row>
-              <Col md={{ span: 1 }}></Col>
-              <Col md={{ span: 10 }}>
-                <strong>
-                  <h1
-                    className="d-flex justify-content-center"
-                    style={{ fontSize: "5rem" }}
-                  >
-                    50$
-                  </h1>
-                </strong>
-                <div className="text-muted d-flex justify-content-center pb-2">
-                  No Nonsense. No Hidden Fees.
-                </div>
-                <div className="d-flex justify-content-center">
-                  <Link to="/checkout">
-                    <Button size="lg" className="mr-3 px-5">
-                      Get started
-                    </Button>
-                  </Link>
-                </div>
-              </Col>
-              <Col md={{ span: 1 }}></Col>
-            </Row>
-          </Container>
-        </div>
+        <MainJumbo />
       </div>
 
       <FAQ />
