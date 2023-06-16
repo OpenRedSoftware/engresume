33ccafd233ab622fd66075a8d3694838f96fc03b
dginovker <danielginovker@gmail.com>
2023-06-16 18:38:32 -0400

    Behavioral questions pass

--- src/pages/interviewguide/behavioral.tsx
+++ src/pages/interviewguide/behavioral.tsx
@@ -12,14 +12,22 @@ function Behavioral() {
     <>
       <Helmet prioritizeSeoTags>
         <title>STAR Format Properly | Pineapple Resume</title>
-        <meta property="og:title" content="STAR Format Properly | Pineapple Resume" />
-        <meta property="og:description" content="Advanced tips on How to use STAR Format - Use this cheat sheet to ace your next interview" />
+        <meta
+          property="og:title"
+          content="STAR Format Properly | Pineapple Resume"
+        />
+        <meta
+          property="og:description"
+          content="Advanced tips on How to use STAR Format - Use this cheat sheet to ace your next interview"
+        />
       </Helmet>
       <div>
         <h2 className="text-center">Behavioral Interview Guide</h2>
         <ul>
           <li className="pb-2">
-            <div className="pb-2">Why Situation, Task, Action, Result Format?</div>
+            <div className="pb-2">
+              Why Situation, Task, Action, Result Format?
+            </div>
             <ul className="pb-2">
               <li>It forces linear story-telling</li>
               <li>Keeps the interviewer engaged</li>
@@ -34,42 +42,39 @@ function Behavioral() {
             <ul className="pb-2">
               <li>
                 Avoid saying “we” when telling stories. Everything should be
-                what you did.
+                what <i>you</i> did.
               </li>
-              <li>Never decline answering a question</li>
-              <li>
-                Practice downwards tone inflection - You don’t want to come off
-                as lacking confidence
-              </li>
+              <li>Never decline answering a question.</li>
             </ul>
           </li>
           <li className="pb-2">
             <div className="pb-2">Prepare stories</div>
             <ul className="pb-2">
-              <li>Do not write out essays</li>
+              <li>Do not write out essays.</li>
               <li>
-                Your stories should be 2-3 words that remind you of a task you
-                completed
+                Your stories should be 2-3 words that <i>remind</i> you of a
+                task you completed.
               </li>
               <li>
-                Group your stories by what sort of question they might be asking
-                about
-              </li>
-              <li>
-                I recommend the Amazon LPs:
-                https://www.amazon.jobs/content/en/our-workplace/leadership-principles
-              </li>
-              <li>
-                If you’re targeting a less senior position, at least group your
-                stories for the main ones (Ownership/Customer Obsession/
+                Group your stories by LPs.
+                <ul>
+                  <li>
+                    If you don't want to follow the Amazon LPs, at least group
+                    by Ownership/Customer Obsession/Dive Deep/Hire & Develop the
+                    Best
+                  </li>
+                  <li>
+                    These groups will cover 90% of the questions you will be
+                    asked in a behavioral interview
+                  </li>
+                </ul>
               </li>
             </ul>
           </li>
         </ul>
         <div className="pb-2">
           For every response, walk through these questions and give the answer.
-          It should take 3-5 minutes maximum. If you answer all these questions
-          in each of your responses, you will crush the interview.
+          It should take 3-5 minutes maximum.
         </div>
         <ul className="pb-2">
           <li>ST-</li>
@@ -97,37 +102,50 @@ function Behavioral() {
           </ul>
         </ul>
 
-        <li className="pb-2">
-          <div className="pb-2">Advanced Tips</div>
-          <ul className="pb-2">
-            <li>
-              The interviewer’s question doesn’t actually matter - What matters
-              is you prove you have the skill they’re looking for in the
-              question
-            </li>
-            <li>
-              Consider the following question: “Tell me about a time you made a
-              hard decision to sacrifice short term gain for a longer-term goal”
-            </li>
-            <ul>
+        <ul>
+          <li className="pb-2">
+            <div className="pb-2">Advanced Tips</div>
+            <ul className="pb-2">
               <li>
-                The interviewer does not really care about the specifics here
+                The interviewer’s question doesn’t actually matter - What
+                matters is you prove you have the skill they’re looking for
               </li>
               <li>
-                The interviewer wants you to prove you have ownership over the
-                products you’re working on
+                Consider the following question: “Tell me about a time you made
+                a hard decision to sacrifice short term gain for a longer-term
+                goal”
               </li>
+              <ul>
+                <li>
+                  The interviewer does not really care about the specifics
+                </li>
+                <li>
+                  The interviewer wants you to prove you have ownership over the
+                  products you’re working on
+                </li>
+              </ul>
             </ul>
-          </ul>
-        </li>
+          </li>
+        </ul>
 
         <div className="pb-2">Great external resources:</div>
         <ul className="pb-2">
           <li>
-            Guess the LP - Fantastic Behavioral questions:{" "} 
+            Guess the LP - In addition to answering the questions above, GuessTheLP tells
+            you what the interviewer wants to know for each LP:{" "}
             <a href="https://guessthelp.com/">guessthelp.com</a>
           </li>
+          <li>
+            Amazon Leadership Principles:{" "}
+            <a href="https://www.amazon.jobs/content/en/our-workplace/leadership-principles">
+              amazon.jobs
+            </a>
+          </li>
         </ul>
+
+        <p className="text-muted">
+          This article focuses is based around Amazon's LPs, but the same principles apply to every company.
+        </p>
       </div>
     </>
   );
