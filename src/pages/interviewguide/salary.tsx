1e94cdd25062058fed4e5939658eccf7d018a9ba
dginovker <danielginovker@gmail.com>
2023-06-16 23:10:13 -0400

    Review salary

--- src/pages/interviewguide/salary.tsx
+++ src/pages/interviewguide/salary.tsx
@@ -12,8 +12,14 @@ function Salary() {
     <>
       <Helmet prioritizeSeoTags>
         <title>How to do Salary Negotiation Properly | Pineapple Resume</title>
-        <meta property="og:title" content="How to do Salary Negotiation Properly | Pineapple Resume" />
-        <meta property="og:description" content="No fluff - These are the steps you must follow to maximize your negotiation and make sure you get the highest compensation for any role." />
+        <meta
+          property="og:title"
+          content="How to do Salary Negotiation Properly | Pineapple Resume"
+        />
+        <meta
+          property="og:description"
+          content="No fluff - These are the steps you must follow to maximize your negotiation and make sure you get the highest compensation for any role."
+        />
         <meta
           property="og:url"
           content="https://www.pineappleresume.com/salary"
@@ -33,21 +39,19 @@ function Salary() {
                   company so much, and can't wait to work here!"
                 </li>
               </ul>
-              <li>You absolutely must negotiate salary</li>
+              <li>You absolutely must negotiate salary.</li>
               <ul>
-                <li>Hiring managers expect this</li>
+                <li>Hiring managers expect this.</li>
                 <li>
-                  It is almost unheard of for an offer to be withdrawn due to
-                  salary negotiation
+                  It is unheard of for an offer to be withdrawn for negotiation.
                 </li>
                 <ul>
                   <li>
-                    Complete unfounded greed and lies can get your offer
-                    withdrawn
+                    Greed, immaturity and lies can get your offer withdrawn.
                   </li>
                   <li>
-                    In hiring of over 1,000 candidates, we've seen only 2 or 3
-                    cases of an offer being withdrawn at the negotiation phase
+                    In over 1,000 candidate hires, we've seen only 2 or 3 cases
+                    of an offer being withdrawn due to negotiation.
                   </li>
                 </ul>
               </ul>
@@ -56,31 +60,34 @@ function Salary() {
           <li>
             <div className="pb-2">Maximizing Salary:</div>
             <ul className="pb-2">
-              <li>Know the comp for this role before you enter negotiation</li>
+              <li>Know the comp for this role before you enter negotiation.</li>
               <ul>
+                <li>Look up the company/role/location on Levels.fyi.</li>
                 <li>
-                  Use Levels.fyi to look it up for the company, role, and
-                  location
-                </li>
-                <li>
-                  The highest salary you can get is at the top of the pay band
-                  for this role
+                  The highest salary you can get is the top of the pay band for
+                  this role.
                 </li>
                 <li>
                   If your exact role/company isn’t available on Levels, find
-                  similar ones
+                  similar ones.
                 </li>
               </ul>
               <li>Do not lie about your current comp</li>
               <ul>
                 <li>
-                  If you don’t want to share your comp (it’s too low), instead
-                  talk about what comp you’d like to see
+                  Don't bring up your current comp unless you have to. Focus on
+                  what comp you would like to see.
                 </li>
               </ul>
               <li>
                 Your frame of mind must be that you adore this company and want
-                to work at it, no matter what
+                to work at it.
+                <ul>
+                  <li>
+                    Your hiring manager needs to feel confident when pitching to
+                    their manager that the number you reach will close the deal.
+                  </li>
+                </ul>
               </li>
               <ul>
                 <li>
@@ -92,17 +99,16 @@ function Salary() {
                 <li>If you give a single number, you’ve capped yourself</li>
                 <li>
                   The lowest number you give must always be the lowest value you
-                  would accept the offer for
+                  would accept.
                 </li>
                 <li>
-                  If you’re not going to join this company for anything less
-                  than 180k, don’t say “160-200” and hope it will somehow work
-                  out. Say “180-200”. Be prepared to accept the lowest number
-                  you give.
+                  If you won't quit your current job for anything less than
+                  180k, don’t say “160-200” and hope it will somehow work out.
+                  Say “180-200”, and be ready to accept 180.
                 </li>
                 <li>
-                  If they can’t match your lowest number, you’ve already messed
-                  up
+                  If they can’t match your lowest number, there was already a
+                  problem.
                 </li>
                 <ul>
                   <li>
@@ -110,11 +116,9 @@ function Salary() {
                   </li>
                   <li>Is this a leveling issue?</li>
                   <li>Are you not understanding all the parts in the TC?</li>
-                  <li>
-                    Can the company just not pay this much, and that difference
-                    is too large to overcome?
-                  </li>
+                  <li>Can the company just not pay this much?</li>
                 </ul>
+                <li>Salary &#62; Stock &#62; Bonus</li>
               </ul>
             </ul>
           </li>
