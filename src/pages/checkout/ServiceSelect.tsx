c43d6c84afde780685d32f4ca730b39afdd03ad6
dginovker <danielginovker@gmail.com>
2023-07-13 20:01:32 -0400

    Add content to service descriptions

--- src/pages/checkout/ServiceSelect.tsx
+++ src/pages/checkout/ServiceSelect.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Form from "react-bootstrap/Form";
 import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "./store";
@@ -6,74 +6,65 @@ import { setService } from "./formSlice";
 import "./ServiceSelect.css";
 
 const ServiceSelect: React.FC = () => {
-  const { service } = useSelector((state: RootState) => state.form);
+  const { service } = useSelector(
+    (state: RootState) => state.form
+  );
+
   const dispatch = useDispatch();
 
-  // Additional state to handle which row is expanded
-  const [expandedService, setExpandedService] = useState("");
+  const serviceDetails = {
+    "basic": (
+      <ul>
+        <li>Feedback on all content, including skills, education, work experience, and projects</li>
+        <li>Feedback on formatting</li>
+        <li>Suggestions to improve bullet points</li>
+      </ul>
+    ),
+    "full": (
+      <ul>
+        <li>All services included in Basic Review</li>
+        <li>Rewritten bullet points where needed</li>
+      </ul>
+    ),
+    "fullRewrite": (
+      <ul>
+        <li>All services included in Full Review</li>
+        <li>All bullet points rewritten</li>
+        <li>New resume from a professional template</li>
+      </ul>
+    )
+  }
 
-  const handleRowClick = (serviceName: string) => {
-    if (service !== serviceName) {
-      dispatch(setService(serviceName));
-      setExpandedService(serviceName); // Expand the clicked row
+  const getCanonicalService = (service: string) => {
+    if (service === "fullRewrite") {
+      return "Full Rewrite";
+    } else {
+      return service.charAt(0).toUpperCase() + service.slice(1) + " Review";
     }
-  };
+  }
 
   return (
     <>
       <Form.Label>Service</Form.Label>
       <Form.Group controlId="serviceSelection">
-        <div className="service-select-container">
-          <div
-            className={`row-select ${service === "basic" ? 'selected-row' : ''} ${expandedService === "basic" ? 'expanded-row' : ''}`}
-            onClick={() => handleRowClick("basic")}
-          >
-            <div className="title-radio-wrapper">
-              <div>Basic Review - $19.99</div>
+        <div className={`service-select-container ${service ? '' : 'is-invalid'}`}>
+          {["basic", "full", "fullRewrite"].map((type) => (
+            <div
+              key={type}
+              className={`row-select ${service === type ? 'selected-row' : ''}`}
+              onClick={() => { if (service !== type) dispatch(setService(type)) }}
+            >
+              <div>{getCanonicalService(type)} - {type === "basic" ? '$19.99' : type === "full" ? '$49.99' : '$349.99'}</div>
               <Form.Check
                 type="radio"
                 name="service"
-                id="basicReview"
-                checked={service === "basic"}
+                id={`${type}Review`}
+                checked={service === type}
                 onChange={() => {}}
               />
+              {service === type && <div className="service-details">{serviceDetails[type]}</div>}
             </div>
-            {expandedService === "basic" && <div className="service-info">This is the basic description</div>}
-          </div>
-
-          <div
-            className={`row-select ${service === "full" ? 'selected-row' : ''} ${expandedService === "full" ? 'expanded-row' : ''}`}
-            onClick={() => handleRowClick("full")}
-          >
-            <div className="title-radio-wrapper">
-              <div>Full Review - $49.99</div>
-              <Form.Check
-                type="radio"
-                name="service"
-                id="fullReview"
-                checked={service === "full"}
-                onChange={() => {}}
-              />
-            </div>
-            {expandedService === "full" && <div className="service-info">This is the full description</div>}
-          </div>
-          
-          <div
-            className={`row-select ${service === "fullRewrite" ? 'selected-row' : ''} ${expandedService === "fullRewrite" ? 'expanded-row' : ''}`}
-            onClick={() => handleRowClick("fullRewrite")}
-          >
-            <div className="title-radio-wrapper">
-              <div>Full Review + Resume Rewrite - $349.99</div>
-              <Form.Check
-                type="radio"
-                name="service"
-                id="fullReviewRewrite"
-                checked={service === "fullRewrite"}
-                onChange={() => {}}
-              />
-            </div>
-            {expandedService === "fullRewrite" && <div className="service-info">This is the full rewrite description</div>}
-          </div>
+          ))}
         </div>
       </Form.Group>
     </>
