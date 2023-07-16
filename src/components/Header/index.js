1821a04c9752ea21895df42932e8faf136b05060
dginovker <danielginovker@gmail.com>
2023-07-16 13:45:52 -0400

    Add pineapple logo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,6 +4,7 @@ import CheckoutButton from "./CheckoutButton";
 import InterviewGuideButton from "./InterviewGuideButton";
 import Theme from "./Theme";
 import { Link } from "react-router-dom";
+import Image from "react-bootstrap/Image";
 
 const Header = (props) => {
   return (
@@ -13,6 +14,12 @@ const Header = (props) => {
           <Link to="/"
             style={{ textDecoration: "none", color: "var(--color-text)" }}
           >
+            <Image
+              src="/res/logo.webp"
+              alt="Pineapple Resume Logo"
+              width={"30em"}
+              style={{ marginRight: "0.3em", paddingBottom: "0.2em"}}
+              />
             Pineapple Resume
           </Link>
         </Navbar.Brand>
